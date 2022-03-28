8ff0234179cbc035130c5df0ceb0c7587375062a
ArindamSengupta <arindam.sg.123@gmail.com>
2022-03-28 11:00:16 +0530

    cloudbuild

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-resource "google_compute_instance" "test-vm" {
-  name         = "test-vm"
-  machine_type = "n1-standard-1"
-  zone         = "asia-southeast1-a"
-
-  network_interface {
-    network = "custom-vpc-tf"
-    subnetwork = "sug-sg"
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-      size = 20
-    }
-  }
+resource "google_service_account" "service_account" {
+  account_id   = "arindambuild9"
+  display_name = "arindamsvc"
 }
+
